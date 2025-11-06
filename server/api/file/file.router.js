a543150fdb3ff9d39587465a6e12d73babed6231
Artem Agaev <artem5agaev@gmail.com>
2025-11-06 15:38:01 +0500

    Add comments to routes

--- server/api/file/file.router.js
+++ server/api/file/file.router.js
@@ -5,11 +5,21 @@ import controller from "./file.controller.js";
 
 const router = Router();
 
+// Every request must be authenticated
+
+// Requires file upload ("file" field)
 router.post("/upload",          authenticate, muter_upload.single("file"), controller.uploadFile);
+
+// Query params: list_size, page
 router.get("/list",             authenticate, controller.listFiles);
+
 router.get("/:id",              authenticate, controller.getFile);
+
 router.get("/download/:id",     authenticate, controller.downloadFile);
+
+// Requires file upload ("file" field)
 router.put("/update/:id",       authenticate, muter_upload.single("file"), controller.updateFile);
+
 router.delete("/delete/:id",    authenticate, controller.deleteFile);
 
 export default router;
--- server/api/user/user.router.js
+++ server/api/user/user.router.js
@@ -4,11 +4,19 @@ import authenticate from "#middlewares/authenticate.middleware.js";
 
 const router = Router();
 
+// Requires authentication
 router.get("/info",                 authenticate,   getInfo);
+
+// Requires authentication
 router.post("/logout",              authenticate,   logout);
 
+// Requires json body with "id" and "password" fields
 router.post("/signup",              signup);
+
+// Requires "refresh_token" cookie
 router.post("/signin/new_token",    refresh);
+
+// Requires json body with "id" and "password" fields
 router.post("/signin",              signin);
 
 export default router;
